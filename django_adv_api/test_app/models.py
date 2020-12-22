ef403fc5d094f0031ecb8a839a06d77d363dbe96
manjeete <manjeetkr2017@gmail.com>
2020-12-22 14:17:13 +0530

    created customuser

--- django_adv_api/test_app/models.py
+++ django_adv_api/test_app/models.py
@@ -1,2 +1,11 @@
 from django.db import models
 
+class Blog(models.Model):
+    titles = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.title)
